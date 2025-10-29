eae8f50ca781bf062e2930b65002d91b28b4490d
Ana Clara Chaves <a7668@esap.edu.pt>
2025-10-29 11:20:40 +0000

    trabalhoPSI

---
+++ TrabalhoGrupoM9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoGrupoM9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+
+            Console.WriteLine("Digite 10 números:");
+
+            // Loop para receber os 10 números do usuário
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Número {i + 1}: ");
+                while (!int.TryParse(Console.ReadLine(), out numeros[i]))
+                {
+                    Console.WriteLine("Por favor, insira um número válido.");
+                    Console.Write($"Número {i + 1}: ");
+                }
+            }
+
+            // Encontrar o maior número
+            int maior = numeros[0];
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                if (numeros[i] > maior)
+                {
+                    maior = numeros[i];
+                }
+            }
+
+            // Exibir o maior número
+            Console.WriteLine($"\nO maior número digitado foi: {maior}");
+            Console.ReadLine();
+        }
+        
+    }
+}
